a9b73b2cdd851d357b28986e6a69c9050b0b44e6
Adriano <62570164+AdrianGeorgeM@users.noreply.github.com>
2023-02-14 20:33:43 +0000

    Finished working files

--- app/src/Escrow.js
+++ app/src/Escrow.js
@@ -1,37 +1,33 @@
-export default function Escrow({
-  address,
-  arbiter,
-  beneficiary,
-  value,
-  handleApprove,
-}) {
-  return (
-    <div className="existing-contract">
-      <ul className="fields">
-        <li>
-          <div> Arbiter </div>
-          <div> {arbiter} </div>
-        </li>
-        <li>
-          <div> Beneficiary </div>
-          <div> {beneficiary} </div>
-        </li>
-        <li>
-          <div> Value </div>
-          <div> {value} </div>
-        </li>
-        <div
-          className="button"
-          id={address}
-          onClick={(e) => {
-            e.preventDefault();
+import { ethers } from 'ethers';
 
-            handleApprove();
-          }}
-        >
-          Approve
-        </div>
-      </ul>
-    </div>
-  );
+export default function Escrow({ address, arbiter, beneficiary, value, handleApprove }) {
+	return (
+		<div className='existing-contract'>
+			<ul className='fields'>
+				<li>
+					<div> Arbiter </div>
+					<div> {arbiter} </div>
+				</li>
+				<li>
+					<div> Beneficiary </div>
+					<div> {beneficiary} </div>
+				</li>
+				<li>
+					<div> Value </div>
+					<div> {ethers.utils.formatEther(value)} </div>
+				</li>
+				<div
+					className='button'
+					id={address}
+					onClick={(e) => {
+						e.preventDefault();
+
+						handleApprove();
+					}}
+				>
+					Approve
+				</div>
+			</ul>
+		</div>
+	);
 }
